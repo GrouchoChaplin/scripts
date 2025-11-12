0708a01e64ef5445f6155a168a678c73cb3fd6c4
Ed Peddycoart <ed.peddycoart@bluehalo.com>
2025-11-12 15:48:32 -0600

    fixed gss, removed gs

--- env/aliases_env.sh
+++ env/aliases_env.sh
@@ -1,6 +1,5 @@
 # scripts/env/aliases.sh
 alias ll='ls -lh --color=auto'
-alias gs='git status'
-alias gss='gs -s'
+alias gss='git status $@'
 alias gcm='git commit -m'
 alias reloadbash='source ~/.bashrc'
