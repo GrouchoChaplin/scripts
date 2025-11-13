a4638721db4420cdce47ceb0f8aece0b702c72e1
Ed Peddycoart <ed.peddycoart@bluehalo.com>
2025-11-13 16:20:39 -0600

    initial version

---
+++ git/forensics/compare_repo_variants_V5.6_Patch6/test.sh
@@ -0,0 +1,5 @@
+./compare_repo_variants_V5.6_Patch6.sh \
+  --root-folder /run/media/peddycoartte/MasterBackup/Nightly/2025-10-10 \
+  --repo-name jsigconversiontools \
+  --mode forensic \
+  --debug-find
